2dfbfc4a10e5c66e08e494efccf86d03655571b2
erikvella <vella.erik1@gmail.com>
2023-09-13 14:52:10 +0100

    [init] js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// 1 prendo tutti gli elementi 
+
+const itemsWrapper = document.querySelector('.items-wrapper');
+const nextBtn = document.querySelector('.up');
+const prevBtn = document.querySelector('.down');
+
+const imgCollection = [
+  'img/01.webp' , 
+  'img/02.webp' , 
+  'img/03.webp' , 
+  'img/04.webp' , 
+  'img/05.webp' 
+];
+
+for(let i = 0 ; i < imgCollection.length ; i++){
+  const image = imgCollection[i];
+}
